cdb34f2f765ad031ff9daa8f81f8a3649e7db29d
Yudhi Jatnika <yudhi.404@gmail.com>
2026-01-29 16:54:28 +0000

    Fix: Membersihkan file yang seharusnya di-ignore

--- vite.config.js
+++ vite.config.js
@@ -1,28 +1,33 @@
 import { fileURLToPath, URL } from 'node:url'
 
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    },
-  },
-  server: {
-    proxy: {
-      '/api': {
-        target: 'https://apistore.wiyatajatidiri.com',
-        changeOrigin: true,
-        secure: true,
-        rewrite: path => path.replace(/^\/api/, ''),
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+
+  return {
+    plugins: [
+      vue(),
+      vueDevTools(),
+    ],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
       },
     },
-  },
+    server: {
+      proxy: {
+        '/api': {
+          target: env.VITE_API_BASE_URL || 'https://apistore.wiyatajatidiri.com',
+          changeOrigin: true,
+          secure: true,
+          rewrite: path => path.replace(/^\/api/, ''),
+        },
+      },
+    },
+  }
 })
+
